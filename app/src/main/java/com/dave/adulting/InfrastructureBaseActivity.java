ed2bd3ba6c14b1f58350ed099ab2e5c8dd27b39a
Dave Parker <DYJParker@gmail.com>
2017-05-16 14:26:29 -0400

    Completed firebase-Auth in abstracted activity

---
+++ app/src/main/java/com/dave/adulting/InfrastructureBaseActivity.java
@@ -0,0 +1,106 @@
+package com.dave.adulting;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.*;
+import com.firebase.ui.auth.BuildConfig;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+/**
+ * Created by Dave - Work on 5/16/2017.
+ */
+
+abstract class InfrastructureBaseActivity extends AppCompatActivity {
+    protected FirebaseAuth mAuth;
+    protected static final int FB_SIGN_IN = 1000;
+    protected Intent mFBSignin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() == null) {
+            mFBSignin = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                    .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                    .build();
+            startActivityForResult(mFBSignin, FB_SIGN_IN);
+        } else {
+            onSpecificCreate();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FB_SIGN_IN) {
+            if (resultCode == ResultCodes.OK) onSpecificCreate();
+            else signInError(IdpResponse.fromResultIntent(data));
+        }
+    }
+
+    protected void signInError(IdpResponse response) {
+        String error;
+        if (response == null) error = "You pressed the back button";
+        else if (response.getErrorCode() == ErrorCodes.NO_NETWORK)
+            error = "You don't currently have internet access to sign in";
+        else error = "An unknown error occurred";
+        Toast.makeText(this, error + ", please try signing in again to access app functionality.", Toast.LENGTH_LONG).show();
+        startActivityForResult(mFBSignin, FB_SIGN_IN);
+    }
+
+    abstract protected void onSpecificCreate();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_common, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_signout) {
+            final AppCompatActivity ctx = this;
+            AuthUI.getInstance()
+                    .signOut(ctx)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            startActivity(new Intent(ctx, ctx.getClass()));
+                            finish();
+                        }
+                    });
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/dave/adulting/PerishableActivity.java
@@ -0,0 +1,30 @@
+package com.dave.adulting;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class PerishableActivity extends InfrastructureBaseActivity {
+    private static final String TAG = "PerishableActivity";
+
+    @Override
+    protected void onSpecificCreate() {
+        setContentView(R.layout.activity_perishable);
+
+        Log.d(TAG, "onSpecificCreate happened!");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
